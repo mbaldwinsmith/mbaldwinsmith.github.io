17229d7e796ed2cdcd949eb4ec0e12c617da916c
Mark Baldwin-Smith <mb110413@gmail.com>
2025-10-17 15:49:26 +0100

    feat: optimize portfolio images with astro assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://mbaldwinsmith.github.io',
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/squoosh',
+    },
+  },
 });
